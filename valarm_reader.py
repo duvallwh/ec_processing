80548970f50414502b8397613905c4fa2bd65694
duvallwh <whduvall@gmail.com>
2018-07-26 12:42:21 -0700

    added to valarm_reader

--- valarm_reader.py
+++ valarm_reader.py
@@ -1,3 +1,5 @@
+"""Loads and parses data from zipped Valarm file in standard
+format"""
 import zipfile
 import os
 import pandas as pd
@@ -24,7 +26,11 @@ class ValarmReader(object):
                           'VOLT_6': 'no_ae', 'VOLT_7': 'sensor_input_voltage',
                           'VOLT_8': 'system_voltage'}
 
-    def determine_sensor(self):
+    def _determine_sensor(self):
+        """
+        Determines which of two Valarm sensors using file_path.
+        """
+        
         if "SanAntonio_01" in self.file_path:
             return "val_site_1"
         
@@ -34,11 +40,33 @@ class ValarmReader(object):
         else:
             return "bad_file_name"
         
-    def open_file(self):
-        pass
+    def _open_file(self):
+        """
+        opens zipped valarm file and loads into pandas dataframe
+        """
 
-    def parse_data(self):
-        pass
+        zf = zipfile.ZipFile(self.file_path, 'r')
+        return pd.read_csv(zf.open(str(zf.namelist()[0])),
+                          parse_dates=['TIME'])
+    
+    def load_data(self):
+        """
+        returns data stored in zipped file listed in file_path
+        """
+        site = self._determine_sensor()
+        if site == "val_site_1":
+            unit_map = self.unit1_map
+        elif site == "val_site_2":
+            unit_map = self.unit2_map
+        else:
+            return 'Could not load data'
+        dat = self._open_file()
+        dat.index = dat['TIME']
+        dat.index = dat.index.tz_localize('UTC').tz_convert('US/Pacific')
+        dat = dat.rename(columns=unit_map)
+        dat = dat[list(unit_map.values())]
+        dat['site'] = site
+        return dat        
 
 ###### to be moved ######
 
@@ -56,15 +84,18 @@ class ValarmReaderTest(unittest.TestCase):
         test_file_path = os.path.join(test_file_folder, test_file)
         self.vfp = val_obj = ValarmReader(test_file_path)
 
-    def test_determine_sensor(self):
-        self.assertEqual("val_site_2", self.vfp.determine_sensor())
-
-    def test_open_file(self):
-        self.assertTrue(type(self.vfp.open_file()) == pd.core.frame.DataFrame)
-
     def tearDown(self):
         print("Destroying the valarm file parser...")
         self.vfp = None           
+
+    def test_determine_sensor(self):
+        self.assertEqual("val_site_2", self.vfp._determine_sensor())
+
+    def test_open_file(self):
+        self.assertTrue(type(self.vfp._open_file()) == pd.core.frame.DataFrame)
+
+    def test_load_data(self):
+        self.assertTrue(type(self.vfp.load_data()) == pd.core.frame.DataFrame)
 
 if __name__=="__main__":
     unittest.main()
